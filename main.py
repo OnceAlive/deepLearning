cbde126dac22e713f6b52c982a97836a747a513b
Tim Göbel <timgoebel148@gmail.com>
2023-04-25 09:56:09 +0200

    final MNIST dataset train

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ plt.savefig('mnist.png')
 train_images = train_images.reshape((60000, 28 * 28))
 train_images.astype('float32') / 255
 
-train_images = train_images.reshape((10000, 28 * 28))
-train_images.astype('float32') / 255
-
-
+test_images = test_images.reshape((10000, 28 * 28))
+test_images.astype('float32') / 255
 
 train_labels = tf.keras.utils.to_categorical(train_labels)
+
+model = tf.keras.models.Sequential([tf.keras.layers.Dense(5000, activation='relu', input_shape=(28 * 28,)), tf.keras.layers.Dense(10, activation='softmax')])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10, batch_size=128)
+model.evaluate(train_images, train_labels)
